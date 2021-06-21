7ba8d2a5f0c6652bc3a7b48b48be288f9c0da017
SStepurko <82845341+SStepurko@users.noreply.github.com>
2021-06-21 18:42:21 +0300

    Update TestPerformance.java

--- src/edu/bu/met622/collections/TestPerformance.java
+++ src/edu/bu/met622/collections/TestPerformance.java
@@ -10,47 +10,52 @@ import java.util.TreeMap;
 import java.util.Vector;
 
 public class TestPerformance {
-  public static void main(String[] args) {
-//	  Instant startV = Instant.now();
-//	  Vector<Integer> a = new Vector();
-//	  for (int i=0; i<1000000; i++) {
-//		  a.add(i);
-//	  }
-//	  Instant stopV = Instant.now();
-//	  System.out.println("--vector--->"+Duration.between(startV, stopV).getNano());
+	public static void main(String[] args) {
+		int lim = 1000000;
 
-	  Instant startQ = Instant.now();
-	  ArrayDeque c = new ArrayDeque(20);
-	  for (int k=0; k<1000000; k++) {
-		  c.add(k);
-	  }
-	  Instant stopQ = Instant.now();
-	  System.out.println("--arrayDeque--->"+Duration.between(startQ, stopQ).getNano());
-  
-	  Instant startA = Instant.now();
-	  ArrayList b = new ArrayList();
-	  for (int j=0; j<1000000; j++) {
-		  b.add(j);
-	  }
-	  Instant stopA = Instant.now();
-	  System.out.println("--arraylist--->"+Duration.between(startA, stopA).getNano());
-	
-	  Instant startHM = Instant.now();
-	  Map<String, String> map = new HashMap<>();
-	  for (int k=0; k<1000000; k++) {
-			 map.put(String.valueOf(k), "x"); 
-	  }
-	  Instant stopHM = Instant.now();
-	  System.out.println("--HashMap--->"+Duration.between(startHM, stopHM).getNano());
-	  
-	  Instant startTM = Instant.now();
-	  Map<String, String> tmap = new TreeMap<>();
-	  for (int k=0; k<1000000; k++) {
-			 tmap.put(String.valueOf(k), "x"); 
-	  }
-	  Instant stopTM = Instant.now();
-	  System.out.println("--TreeMap--->"+Duration.between(startTM, stopTM).getNano());
-	    
+		Instant startV = Instant.now();
+		Vector<Integer> vector = new Vector();
+		for (int i=0; i<lim; i++) {
+			vector.add(i);
+		}
+		Instant stopV = Instant.now();
+		System.out.println("--vector--->"+Duration.between(startV, stopV).getNano());
+		vector = null;
 
-  }
+		Instant startQ = Instant.now();
+		ArrayDeque arrayDeque = new ArrayDeque();
+		for (int k=0; k<lim; k++) {
+			arrayDeque.add(k);
+		}
+		Instant stopQ = Instant.now();
+		System.out.println("--arrayDeque--->"+Duration.between(startQ, stopQ).getNano());
+		arrayDeque = null;
+
+		Instant startA = Instant.now();
+		ArrayList arrayList = new ArrayList();
+		for (int j=0; j<lim; j++) {
+			arrayList.add(j);
+		}
+		Instant stopA = Instant.now();
+		System.out.println("--arraylist--->"+Duration.between(startA, stopA).getNano());
+		arrayList = null;
+
+		Instant startHM = Instant.now();
+		Map<String, String> hashMap = new HashMap<>();
+		for (int k=0; k<lim; k++) {
+				hashMap.put(String.valueOf(k), "x"); 
+		}
+		Instant stopHM = Instant.now();
+		System.out.println("--HashMap--->"+Duration.between(startHM, stopHM).getNano());
+		hashMap = null;
+		
+		Instant startTM = Instant.now();
+		Map<String, String> treeMap = new TreeMap<>();
+		for (int k=0; k<lim; k++) {
+				treeMap.put(String.valueOf(k), "x"); 
+		}
+		Instant stopTM = Instant.now();
+		System.out.println("--TreeMap--->"+Duration.between(startTM, stopTM).getNano());
+		treeMap = null;
+	}
 }
